10c88dd03da2bc4b8c8e15b9a2bc035fdb1adf3a
hiro21 <hiswing8@gmail.com>
2017-10-31 01:16:47 +0900

    change japanpost add download zip

--- japanpost/japanpost/spiders/japan_post_spider.py
+++ japanpost/japanpost/spiders/japan_post_spider.py
@@ -2,6 +2,10 @@ import scrapy
 import re
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
+
+import shutil
+import requests
+
 class JapanPostSpider(CrawlSpider):
     name = 'japanpost'
     start_urls = [
@@ -13,30 +17,25 @@ class JapanPostSpider(CrawlSpider):
     ]
 
     def parse(self, response):
-        print("out response")
-        print(response)
-        print(self)
 
         for url in response.css('a::attr(href)'):
-            #print(url)
-            #print(url.extract())
 
             if url.extract() == "/zipcode/dl/oogaki-zip.html":
-                print("target")
-                # 対象の場合、処理を実行
-                print(response.urljoin(url.extract() ))
                 nextUrl = response.urljoin(url.extract() )
                 yield scrapy.Request(nextUrl, self.parse_nextUrl)
 
     def parse_nextUrl(self, response):
-        #pattern = r'*zip$'
-        #repatter = re.compile(pattern)
-        #re.search(r'*zip$', )
 
         for url in response.css('a::attr(href)'):
-            #matchObj = repatter.search(url.extract())
-            #print(matchObj)
-            m = re.search(re.escape(r'*.zip$'), url.extract())
-            print(m)
-            #if "zip" in url.extract():
-            #    print(url.extract())
+
+            if url.extract() == "oogaki/zip/ken_all.zip":
+                downloadUrl = response.urljoin(url.extract())
+
+                file_name = downloadUrl.split("/")[-1]
+
+                r = requests.get(downloadUrl)
+                if r.status_code == 200:
+                    f = open(file_name, 'wb')
+                    f.write(r.content)
+                    f.close()
+
